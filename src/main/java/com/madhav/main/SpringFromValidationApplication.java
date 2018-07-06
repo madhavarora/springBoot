5a3c52485a49c18d1ce7e478fbc4c91ea8106424
Madhav Arora <maddyarora@yahoo.com>
2018-07-05 23:13:29 -0400

    second commit

--- src/main/java/com/madhav/main/SpringFromValidationApplication.java
+++ src/main/java/com/madhav/main/SpringFromValidationApplication.java
@@ -2,12 +2,18 @@ package com.madhav.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"com.madhav"})
-public class SpringFromValidationApplication {
+public class SpringFromValidationApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringFromValidationApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFromValidationApplication.class, args);
 	}
